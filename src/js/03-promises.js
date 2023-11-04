de8f70922bb2407ced3250be820365bda82e4daf
Ficefi <139962804+Ficefi@users.noreply.github.com>
2023-11-04 09:57:49 +0200

    init

---
+++ src/js/03-promises.js
@@ -0,0 +1,40 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+
+function submitForm(event) {
+  event.preventDefault();
+
+  let inputDelay = event.currentTarget.delay.valueAsNumber;
+  const inputStep = event.currentTarget.step.valueAsNumber;
+  const inputAmount = event.currentTarget.amount.valueAsNumber;
+
+  for (let position = 1; position <= inputAmount; position++){
+    createPromise(position, inputDelay);
+    inputDelay += inputStep;
+  }
+}
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✔️ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+form.addEventListener('submit', submitForm);
